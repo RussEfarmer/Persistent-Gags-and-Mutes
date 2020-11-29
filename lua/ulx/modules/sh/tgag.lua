09e5857136e2aeb1afd40d045d88708904943f41
RussEfarmer <RussEfarmer@gmail.com>
2020-11-28 22:14:56 -0500

    Update debugging stuff

--- lua/ulx/modules/sh/tgag.lua
+++ lua/ulx/modules/sh/tgag.lua
@@ -1,6 +1,6 @@
 
 -- Created by RussEfarmer on 11/26/2020 for Dinklebergs Gmod
--- These commands are adapted from the votemute and votegag commands in cobalt77's "Custom-ULX-Commands" package (https://github.com/cobalt77/Custom-ULX-Commands)
+-- These commands are adapted from the votemute, votegag and pgag commands in cobalt77's "Custom-ULX-Commands" package (https://github.com/cobalt77/Custom-ULX-Commands)
 
 --Hook to gag players
 hook.Add("PlayerCanHearPlayersVoice", "tgaghook", function(listener, talker)
@@ -11,9 +11,9 @@ end)
 --ULX tgag command
 function ulx.tgag( calling_ply, target_ply, minutes)
 	minutes = math.ceil(minutes)
-	print("Command states player should have "..minutes.." gag minutes")
+	ULib.tsay( nil, "Command states player should have "..minutes.." gag minutes")
 	target_ply:SetPData("tgagged", minutes)
-	print("Player now has "..tostring(target_ply:GetPData("tgagged")).." gag minutes on record")
+	ULib.tsay( nil, "Player now has "..tostring(target_ply:GetPData("tgagged")).." gag minutes on record")
 	target_ply.tgagged = true
 	ulx.fancyLogAdmin( calling_ply, "#A has gagged #T for #i minute(s)", target_ply, minutes )
 end
@@ -25,18 +25,17 @@ tgag:help( "Gags a player for a number of minutes." )
 
 --Timed gag logic
 timer.Create( "tgagtimer", 60, 0, function()
-	print("Timer \"tgagtimer\" has fired!")
 	for k,v in pairs( player.GetAll() ) do
 	if ( not v or not IsValid(v) ) then return end
 		local gag = v:GetPData( "tgagged" ) 
 		if ( gag and gag != "0") then
 			if ( not v.tgagged ) then
 				v.tgagged = true
-				print("Player was missing tgagged attribute")
+				ULib.tsay( nil, "Player was missing tgagged attribute")
 			end
-			print("Player had "..tostring(v:GetPData("tgagged")).." gag minutes")
+			ULib.tsay( nil, "Player had "..tostring(v:GetPData("tgagged")).." gag minutes")
 			v:SetPData( "tgagged", tonumber( gag ) - 1 )
-			print("Player now has "..tostring(v:GetPData("tgagged")).." gag minutes")
+			ULib.tsay( nil, "Player now has "..tostring(v:GetPData("tgagged")).." gag minutes")
 			timer.Simple( 0.5, function()
 				local gag = v:GetPData( "tgagged" )
 				if gag == "0" then
@@ -54,10 +53,10 @@ function ulx.untgag( calling_ply, target_plys )
 	ulx.fancyLogAdmin( calling_ply, "#A ungagged #T", target_plys )
 	for k,v in pairs( target_plys ) do
 		if ( v:GetPData( "tgagged" ) and v:GetPData( "tgagged" ) ~= 0 and v:GetPData( "tgagged" ) ~= "0" ) then
-			print("Player has "..tostring(v:GetPData("tgagged")).." gag minutes")
+			ULib.tsay( nil, "Player has "..tostring(v:GetPData("tgagged")).." gag minutes")
 			v:RemovePData( "tgagged" )
 			v.tgagged = nil
-			print("Player now has "..tostring(v:GetPData("tgagged")).." gag minutes")
+			ULib.tsay( nil, "Player now has "..tostring(v:GetPData("tgagged")).." gag minutes")
 		end
 	end
 end
@@ -65,3 +64,19 @@ local untgag = ulx.command( "Chat", "ulx untgag", ulx.untgag, "!untgag", false)
 untgag:addParam{ type=ULib.cmds.PlayersArg }
 untgag:defaultAccess( ULib.ACCESS_ADMIN )
 untgag:help( "Ungag the player" )
+
+--ULX printtgags command
+function ulx.printtgags(calling_ply)
+	local timedGaggedPlayers = {}
+	
+	for k,v in pairs(player.GetHumans()) do
+		if v:GetPData("tgagged") then
+			table.insert(timedGaggedPlayers, v:Nick())
+		end
+	end
+	local message = table.concat(timedGaggedPlayers, ", ")
+	ulx.fancyLog({calling_ply}, "Players currently with gags: #s", message)
+end
+local printtgags = ulx.command( "Chat", "ulx printtgags", ulx.printtgags, "!printtgags", true )
+printtgags:defaultAccess( ULib.ACCESS_ADMIN )
+printtgags:help("Lists players who are connected and have gags.")
