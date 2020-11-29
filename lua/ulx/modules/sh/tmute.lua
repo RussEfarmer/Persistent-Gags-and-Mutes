1142e7c6d9dfc85749d8f86ea28236c57976ff39
RussEfarmer <RussEfarmer@gmail.com>
2020-11-28 21:53:07 -0500

    Added command to print players currently muted

--- lua/ulx/modules/sh/tmute.lua
+++ lua/ulx/modules/sh/tmute.lua
@@ -3,12 +3,14 @@
 -- These commands are adapted from the votemute and votegag commands in cobalt77's "Custom-ULX-Commands" package (https://github.com/cobalt77/Custom-ULX-Commands)
 
 --Hook to mute player
-hook.Add( "PlayerSay", "tmutehook", function(ply)
-	if (ply:GetPData( "tmuted" ) and ply:GetPData( "tmuted" ) ~= 0 and ply:GetPData( "tmuted" ) ~= "0" ) then
-		return ""
-	end
-end)
-
+if SERVER then
+	hook.Remove("PlayerSay", "tmutehook")
+	hook.Add( "PlayerSay", "tmutehook", function(ply)
+		if (ply:GetPData( "tmuted" ) and ply:GetPData( "tmuted" ) ~= 0 and ply:GetPData( "tmuted" ) ~= "0" ) then
+			return ""
+		end
+	end)
+end
 --ULX tmute command
 function ulx.tmute( calling_ply, target_ply, minutes)
 	minutes = math.ceil(minutes)
@@ -37,7 +39,7 @@ timer.Create( "tmutetimer", 60, 0, function()
 	end
 end )
 
---ULX Untmute Command
+--ULX untmute command
 function ulx.untmute( calling_ply, target_plys )
 	ulx.fancyLogAdmin( calling_ply, "#A unmuted #T", target_plys )
 	for k,v in pairs( target_plys ) do
@@ -50,3 +52,22 @@ local untmute = ulx.command( "Chat", "ulx untmute", ulx.untmute, "!untmute" )
 untmute:addParam{ type=ULib.cmds.PlayersArg }
 untmute:defaultAccess( ULib.ACCESS_ADMIN )
 untmute:help( "Unmute the player" )
+
+--ULX printtmutes command
+function ulx.printtmutes(calling_ply)
+	local timedMutedPlayers = {}
+	
+	for k,v in pairs(player.GetHumans()) do
+		if v:GetPData("tmuted") then
+			table.insert(timedMutedPlayers, v:Nick())
+		end
+	end
+	
+	local message = table.concat(timedMutedPlayers, ", ")
+	ulx.fancyLog({calling_ply}, "Players currently with mutes: #s", message)
+end
+
+
+local printtmutes = ulx.command( "Chat", "ulx printtmutes", ulx.printtmutes, "!printtmutes", true )
+printtmutes:defaultAccess( ULib.ACCESS_ADMIN )
+printtmutes:help("Lists players who are connected and have mutes.")
